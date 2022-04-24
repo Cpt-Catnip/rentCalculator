36ec44784dc79716afcc73cfbba0ff32abfb822c
Michael Koshakow <mzkosh@gmail.com>
2022-04-24 12:09:16 -0400

    formats money

--- src/formatAmount.mjs
+++ src/formatAmount.mjs
@@ -4,15 +4,13 @@ const formatAmount = (amount) => {
   // determine order of magnitude
   const orderOfMag = maxPower(amount);
 
-  // number of commas is function of OoM
-  const nCommas = Math.floor(orderOfMag / 3);
-  console.log(`${amount} gets ${nCommas} comma(s)`);
-
   // inser commas
-  const formatted = amount.toFixed(2);
-  const c = formatted.indexOf(".");
-  for (let i = c; i >= 0; i -= 3) {
-    console.log(i);
+  let formatted = amount.toFixed(2);
+  for (let i = orderOfMag - 1; i >= 0; i--) {
+    if (!(i % 3)) {
+      const idx = i + 1;
+      formatted = formatted.slice(0, idx) + "," + formatted.slice(idx);
+    }
   }
 
   // return string
--- src/formatAmount.spec.js
+++ src/formatAmount.spec.js
@@ -1,17 +1,17 @@
 import test from "ava";
 import formatAmount from "./formatAmount.mjs";
 
-test.failing("Adds commas", (t) => {
-  t.is(formatAmount(1234.53), "$1,000.53");
+test("Adds commas", (t) => {
+  t.is(formatAmount(1234.53), "$1,234.53");
   t.is(formatAmount(1000000.03), "$1,000,000.03");
 });
 
-test.failing("Adds a decimal", (t) => {
+test("Adds a decimal", (t) => {
   t.is(formatAmount(1000), "$1,000.00");
   t.is(formatAmount(1), "$1.00");
 });
 
-test.failing("Rounds to the nearest cent", (t) => {
+test("Rounds to the nearest cent", (t) => {
   t.is(formatAmount(1000.118), "$1,000.12");
   t.is(formatAmount(1000.999), "$1,001.00");
 });
